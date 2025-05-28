5e9aeb9649a5f7f6c05de3d58bef0ab651770816
User <alvpermoh@alum.us.es>
2025-05-28 15:45:12 +0000

    Más cambios después del último commit

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from setuptools import setup
-
+import os
+from glob import glob
 package_name = 'dron_misil'
 
 setup(
@@ -10,6 +11,15 @@ setup(
         ('share/ament_index/resource_index/packagetype', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         ('share/' + package_name + '/launch', ['launch/dron_misil_launch.py']),
+                # --- AÑADE ESTAS LÍNEAS ---
+        # Ruta de destino: install/<pkg_name>/share/<pkg_name>/mejores_modelos
+        # Ruta de origen: src/<pkg_name>/<pkg_name>/mejores_modelos
+        (os.path.join('share', package_name, 'mejores_modelos'), glob(os.path.join(package_name, 'mejores_modelos', '*.*'))),
+        # Si tienes subcarpetas dentro de mejores_modelos, necesitarías una estrategia más avanzada
+        # como usar 'include_package_data=True' con MANIFEST.in, o copiar la carpeta completa.
+        # Para una sola carpeta de modelos, glob('*.*') suele bastar si solo hay archivos.
+        # Si 'best_model.zip' está directamente en 'mejores_modelos', esto funciona.
+        # --- FIN AÑADIDO ---
     ],
     install_requires=[
         'setuptools',      # Asegúrate de que setuptools esté allí
